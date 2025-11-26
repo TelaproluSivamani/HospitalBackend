1f995c7e56d0f6233c29c2c1bfbccb30593cc462
TelaproluSivamani <sivamanitelaprolu70@gmail.com>
2025-11-26 15:55:26 +0530

    changed dependencies

--- src/main/java/com/HealthCare/Management/ManagementApplication.java
+++ src/main/java/com/HealthCare/Management/ManagementApplication.java
@@ -2,12 +2,24 @@ package com.HealthCare.Management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * Make the application extend SpringBootServletInitializer so it can be deployed
+ * as a WAR to an external servlet container (Tomcat).
+ */
 @SpringBootApplication
-public class ManagementApplication {
+public class ManagementApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ManagementApplication.class, args);
-	}
+    // Used when the application is launched by the servlet container (WAR)
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ManagementApplication.class);
+    }
 
+    // Used when you run the application standalone via `java -jar`
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementApplication.class, args);
+    }
 }
